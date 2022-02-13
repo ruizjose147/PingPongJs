9ee40319602f981a80ccc8dbc06cd34cdae2f527
JoseRuiz <ruizjose_147@hotmail.com>
2022-02-13 09:27:10 -0500

    se agregan las barras laterales

--- main.js
+++ main.js
@@ -1,30 +1,71 @@
 (function(){
-    var self.Board = function(width, height){
+    self.Board = function(width,height){
         this.width = width;
         this.height = height;
         this.playing = false;
         this.game_over = false;
         this.bars = [];
-        this.ball = null;
+        this.ball = null; 
     }
-
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
+
+        }
+    }
+})();
+
+
+
+(function(){
+    self.BoardView = function(canvas,Board){
+        this.canvas = canvas;
+        this.canvas.width = Board.width;
+        this.canvas.height = Board.height;
+        this.board = Board;
+        this.ctx = canvas.getContext('2d');
+    }
+
+    self.BoardView.prototype = {
+        draw: function(){
+            for ( var i = this.board.elements.length - 1; i >= 0; i--) {
+                var el = this.board.elements[i]
+
+                draw(this.ctx,el);
+            };
+        }
+    }
+
+    function draw(ctx,element){
+        if(element != null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x,element.y,element.width,element.height);
+                    break;
+                }
         }
     }
 
 })();
+ 
 
 (function(){
-    self.BoardView = function(canvas,board){
-        this.canvas = canvas;
-        this.canvas.width = board.width;
-        this.canvas.height = board.height;
+    self.Bar = function(x,y,width,height,board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
         this.board = board;
-        this.ctx = canvas.getContext("2d");
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+    }
+    self.Bar.prototype ={
+        down: function(){
+        },
+        up: function(){
+        }
     }
 })();
 
@@ -32,6 +73,9 @@ window.addEventListener("load",main);
 
 function main(){
     var board = new Board(800,400);
-    var canvas = document.getElementById('canvas');
-    var BoardView = new BoardView(canvas,board);
+    var bar = new Bar(20,100,20,100,board);
+    var bar = new Bar(760,100,20,100,board);
+    var canvas = document.getElementById("canvas");
+    var board_view = new BoardView(canvas,board);
+    board_view.draw();
 }
