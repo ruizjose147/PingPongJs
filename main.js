cce86c32929ff2d56e6c7d1314c4184d38acf3ba
JoseRuiz <ruizjose_147@hotmail.com>
2022-02-13 09:56:30 -0500

    se agrego el escuchador de eventos para las teclas up y down que subiran y bajaran las barras laterales

--- main.js
+++ main.js
@@ -60,22 +60,46 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
+        this.speed = 10;
     }
     self.Bar.prototype ={
         down: function(){
+            this.y += this.speed;
         },
         up: function(){
+            this.y -= this.speed;
+        },
+        toString: function(){
+           return "x: " + this.x + "y: " + this.y; 
         }
     }
 })();
 
-window.addEventListener("load",main);
-
-function main(){
     var board = new Board(800,400);
     var bar = new Bar(20,100,20,100,board);
-    var bar = new Bar(760,100,20,100,board);
+    var bar2 = new Bar(760,100,20,100,board);
     var canvas = document.getElementById("canvas");
     var board_view = new BoardView(canvas,board);
+
+document.addEventListener("keydown",function(ev){
+
+    if(ev.keyCode==38){
+        ev.preventDefault();
+        bar.up();
+
+    }
+
+    else if(ev.keyCode==40){
+        ev.preventDefault();
+        bar.down();
+    }
+
+    console.log(bar.toString());
+});
+
+self.addEventListener("load",main);
+
+function main(){
+    
     board_view.draw();
 }
