e6268b0316782bb87af4958c2fd30e26d834e291
JoseRuiz <ruizjose_147@hotmail.com>
2022-02-13 11:28:11 -0500

    se completa el movimiento de las barras laterales y se agrega la pelota

--- main.js
+++ main.js
@@ -17,7 +17,19 @@
     }
 })();
 
+(function(){
+    self.Ball = function(x, y, radius, board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
 
+        board.ball = this;
+        this.kind = "circle";
+    }
+})();
 
 (function(){
     self.BoardView = function(canvas,Board){
@@ -29,22 +41,33 @@
     }
 
     self.BoardView.prototype = {
+        clean: function(){
+            this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+        },
         draw: function(){
             for ( var i = this.board.elements.length - 1; i >= 0; i--) {
                 var el = this.board.elements[i]
 
                 draw(this.ctx,el);
             };
+        },
+        play: function(){
+            this.clean();
+            this.draw();
         }
     }
 
     function draw(ctx,element){
-        if(element != null && element.hasOwnProperty("kind")){
-            switch(element.kind){
-                case "rectangle":
-                    ctx.fillRect(element.x,element.y,element.width,element.height);
-                    break;
-                }
+        switch(element.kind){
+            case "rectangle":
+                ctx.fillRect(element.x,element.y,element.width,element.height);
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
     }
 
@@ -76,30 +99,46 @@
 })();
 
     var board = new Board(800,400);
-    var bar = new Bar(20,100,20,100,board);
-    var bar2 = new Bar(760,100,20,100,board);
+    var bar = new Bar(760,100,20,100,board);
+    var bar2 = new Bar(20,100,20,100,board);
     var canvas = document.getElementById("canvas");
     var board_view = new BoardView(canvas,board);
+    var ball = new Ball(350, 100, 10, board);
+    
 
-document.addEventListener("keydown",function(ev){
-
+    document.addEventListener("keydown",function(ev){
+    ev.preventDefault();
     if(ev.keyCode==38){
         ev.preventDefault();
         bar.up();
 
     }
 
-    else if(ev.keyCode==40){
+    else if(ev.keyCode===40){
         ev.preventDefault();
         bar.down();
     }
 
-    console.log(bar.toString());
+    else if(ev.keyCode===87){
+        ev.preventDefault();
+        bar2.up();
+    }
+
+    else if(ev.keyCode===83){
+        ev.preventDefault();
+        bar2.down();
+    }
+
+    else if (ev.keyCode===32){
+        ev.preventDefault();
+        board.playing =!board.playing;
+    }
+
 });
 
-self.addEventListener("load",main);
+window.requestAnimationFrame(controller);
 
-function main(){
-    
-    board_view.draw();
+function controller(){
+    board_view.play();
+    window.requestAnimationFrame(controller);
 }
